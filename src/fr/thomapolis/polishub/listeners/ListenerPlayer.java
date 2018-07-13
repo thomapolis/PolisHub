3fd596b6eb83247cb36cb5534217c7e6fbd4aa53
thomapolis <thomapolis@gmail.com>
2018-07-14 00:53:07 +0200

    add text component

--- src/fr/thomapolis/polishub/listeners/ListenerPlayer.java
+++ src/fr/thomapolis/polishub/listeners/ListenerPlayer.java
@@ -8,6 +8,10 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 import fr.thomapolis.poliscore.polisclass.PolisPlayer;
 import fr.thomapolis.polishub.PolisHub;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.HoverEvent.Action;
+import net.md_5.bungee.api.chat.TextComponent;
 
 public class ListenerPlayer implements Listener {
 
@@ -22,7 +26,14 @@ public class ListenerPlayer implements Listener {
 		
 		Player player = event.getPlayer();
 		PolisPlayer polisPlayer = polisHub.getAPI().getPolisPlayer(player);
-		String name = polisPlayer.getRank().getPrefix()+polisPlayer.getCosmeticRank().getPrefix()+" "+polisPlayer.getRank().getColorName()+player.getName();
+		
+		TextComponent tc = new TextComponent();
+		tc.setText(polisPlayer.getCosmeticRank().getSymbole());
+		tc.setColor(net.md_5.bungee.api.ChatColor.valueOf(polisPlayer.getCosmeticRank().getColor().name()));
+		tc.setHoverEvent(new HoverEvent(Action.SHOW_TEXT, new ComponentBuilder(ChatColor.DARK_AQUA+"Le joueur possède le grade "+polisPlayer.getCosmeticRank().getName()).create()));
+
+		
+		String name = polisPlayer.getRank().getPrefix()+tc+" "+polisPlayer.getRank().getColorName()+player.getName();
 		
 		event.setJoinMessage(polisHub.getPrefix()+name+ChatColor.GOLD+" a rejoint le hub !");
 		player.setDisplayName(name);

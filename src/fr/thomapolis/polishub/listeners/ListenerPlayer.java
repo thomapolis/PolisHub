7471faf13ebcabb76e297cc1316efb646fcee377
thomapolis <thomapolis@gmail.com>
2018-07-14 01:00:48 +0200

    Fix bug

--- src/fr/thomapolis/polishub/listeners/ListenerPlayer.java
+++ src/fr/thomapolis/polishub/listeners/ListenerPlayer.java
@@ -8,10 +8,6 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 import fr.thomapolis.poliscore.polisclass.PolisPlayer;
 import fr.thomapolis.polishub.PolisHub;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.HoverEvent;
-import net.md_5.bungee.api.chat.HoverEvent.Action;
-import net.md_5.bungee.api.chat.TextComponent;
 
 public class ListenerPlayer implements Listener {
 
@@ -27,13 +23,7 @@ public class ListenerPlayer implements Listener {
 		Player player = event.getPlayer();
 		PolisPlayer polisPlayer = polisHub.getAPI().getPolisPlayer(player);
 		
-		TextComponent tc = new TextComponent();
-		tc.setText(polisPlayer.getCosmeticRank().getSymbole());
-		tc.setColor(net.md_5.bungee.api.ChatColor.valueOf(polisPlayer.getCosmeticRank().getColor().name()));
-		tc.setHoverEvent(new HoverEvent(Action.SHOW_TEXT, new ComponentBuilder(ChatColor.DARK_AQUA+"Le joueur possède le grade "+polisPlayer.getCosmeticRank().getName()).create()));
-
-		
-		String name = polisPlayer.getRank().getPrefix()+tc+" "+polisPlayer.getRank().getColorName()+player.getName();
+		String name = polisPlayer.getRank().getPrefix()+polisPlayer.getCosmeticRank().getPrefix()+" "+polisPlayer.getRank().getColorName()+player.getName();
 		
 		event.setJoinMessage(polisHub.getPrefix()+name+ChatColor.GOLD+" a rejoint le hub !");
 		player.setDisplayName(name);

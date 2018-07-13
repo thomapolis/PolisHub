42af56d3eb298c7f0bd0fd2edd180ca94aa30f3e
thomapolis <thomapolis@gmail.com>
2018-07-13 20:27:28 +0200

    Work on PolisHub

--- src/fr/thomapolis/polishub/PolisHub.java
+++ src/fr/thomapolis/polishub/PolisHub.java
@@ -1,7 +1,30 @@
 package fr.thomapolis.polishub;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PolisHub extends JavaPlugin {
 
+	private String prefix;
+	
+	@Override
+	public void onEnable() {
+		
+		//Init
+		setPrefix(ChatColor.GRAY+"["+ChatColor.GREEN+"PolisHub"+ChatColor.GRAY+"] ");
+		
+		getServer().getConsoleSender().sendMessage(getPrefix()+"PolisHub is enable !");
+		
+	}
+	
+	public String getPrefix() {
+		
+		return this.prefix;
+	}
+	
+	public void setPrefix(String prefix) {
+		
+		this.prefix = prefix;
+	}
+	
 }

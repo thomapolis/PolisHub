3d1d713dfb2589ea01b0ccbf9c2af40d383b5bc3
thomapolis <thomapolis@gmail.com>
2018-07-14 10:39:00 +0200

    Fix bug

--- src/fr/thomapolis/polishub/gui/GUICosmetics.java
+++ src/fr/thomapolis/polishub/gui/GUICosmetics.java
@@ -27,14 +27,14 @@ public class GUICosmetics extends GUI {
 		ItemMeta gM1 = g1.getItemMeta();
 		gM1.setDisplayName(" ");
 		g1.setItemMeta(gM1);
-		int[] g1S = new int[] {0,1,2,6,7,8,9,17,18,26,27,35,36,37,44,45,46,47,51,52,53};
+		int[] g1S = new int[] {0,1,2,6,7,8,9,17,18,26,27,35,36,44,45,46,47,51,52,53};
 		addItemInventory(inventory, g1, g1S);
 		
 		ItemStack g2 = new ItemStack(Material.STAINED_GLASS_PANE, 1, (byte)4);
 		ItemMeta gM2 = g2.getItemMeta();
 		gM2.setDisplayName(" ");
 		g2.setItemMeta(gM2);
-		int[] g2S = new int[] {3,4,5,10,16, 19, 25, 28, 34, 39, 40, 41};
+		int[] g2S = new int[] {3,4,5,10,16, 19, 25, 28, 34, 37, 39, 40, 41, 48, 49, 50};
 		addItemInventory(inventory, g2, g2S);
 		
 		ItemStack g3 = new ItemStack(Material.STAINED_GLASS_PANE, 1, (byte)3);

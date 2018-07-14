2118df23ebf41637a4f9bdbd614749b48f63a2b4
thomapolis <thomapolis@gmail.com>
2018-07-14 17:57:43 +0200

    Work on Gui

--- src/fr/thomapolis/polishub/gui/GUIGadget.java
+++ src/fr/thomapolis/polishub/gui/GUIGadget.java
@@ -1,7 +1,13 @@
 package fr.thomapolis.polishub.gui;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import fr.thomapolis.polishub.itemseffect.IEBackGUI;
+import fr.thomapolis.polishub.itemseffect.IECloseGUI;
 
 public class GUIGadget extends GUI {
 
@@ -19,7 +25,21 @@ public class GUIGadget extends GUI {
 
 	@Override
 	public void setupInventory(Inventory inventory) {
-		// XXX Auto-generated method stub
+		
+		ItemStack g1 = new ItemStack(Material.STAINED_GLASS_PANE, 1, (byte)15);
+		ItemMeta gM1 = g1.getItemMeta();
+		gM1.setDisplayName(" ");
+		g1.setItemMeta(gM1);
+		int[] g1S = new int[] {0,1,2,6,7,8,46,47,48,50,51,52};
+		addItemInventory(inventory, g1, g1S);
+		
+		IECloseGUI ieCG = new IECloseGUI();
+		inventory.setItem(45, ieCG.getItem());
+		
+
+		
+		IEBackGUI ieBG = new IEBackGUI();
+		inventory.setItem(53, ieBG.getItem());
 		
 	}
 

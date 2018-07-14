a21cbfa04a9d7c1d42b335c95841f7ce491ae6dd
thomapolis <thomapolis@gmail.com>
2018-07-14 14:42:29 +0200

    Work on TenueType

---
+++ src/fr/thomapolis/polishub/type/TenueType.java
@@ -0,0 +1,24 @@
+package fr.thomapolis.polishub.type;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.thomapolis.polishub.tenues.Tenue;
+
+public class TenueType {
+
+public List<Tenue> tenues = new ArrayList<>();
+	
+	public TenueType() {
+		
+		register();
+		
+	}
+	
+	public void register() {
+		
+		
+		
+	}
+	
+}

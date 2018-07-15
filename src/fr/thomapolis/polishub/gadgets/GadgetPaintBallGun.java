72ad2a6ae73750b59feee9bba621ba6c0ef09a76
thomapolis <thomapolis@gmail.com>
2018-07-15 21:37:44 +0200

    Work on gadgetPaintBall

--- src/fr/thomapolis/polishub/gadgets/GadgetPaintBallGun.java
+++ src/fr/thomapolis/polishub/gadgets/GadgetPaintBallGun.java
@@ -1,8 +1,14 @@
 package fr.thomapolis.polishub.gadgets;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import fr.thomapolis.poliscore.cosmeticranks.CosmeticRank;
 
@@ -16,19 +22,31 @@ public class GadgetPaintBallGun extends Gadget {
 
 	@Override
 	public ItemStack getItem() {
-		// XXX Auto-generated method stub
-		return null;
+		
+		List<String> lores = new ArrayList<>();
+		lores.add(ChatColor.GOLD+"Faites clic droit et envoyé une boule");
+		lores.add(ChatColor.GOLD+"de neige qui repeint les blocs qu'elle");
+		lores.add(ChatColor.GOLD+"touche");
+		
+		ItemStack item = new ItemStack(Material.IRON_BARDING, 1);
+		ItemMeta itemMeta = item.getItemMeta();
+		itemMeta.setDisplayName(getName());
+		itemMeta.setLore(lores);
+		item.setItemMeta(itemMeta);
+		
+		return item;
 	}
 
 	@Override
 	public int getCooldown() {
-		// XXX Auto-generated method stub
-		return 0;
+
+		return 10;
 	}
 
 	@Override
 	public void onInteract(Player player) {
-		// XXX Auto-generated method stub
+		
+		player.launchProjectile(Snowball.class, player.getLocation().getDirection());
 
 	}
 

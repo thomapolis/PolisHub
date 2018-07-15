8bea3ff9fa682087e66b2028bc9c5109987d37be
thomapolis <thomapolis@gmail.com>
2018-07-15 20:19:37 +0200

    Work on gadget

--- src/fr/thomapolis/polishub/gadgets/Gadget.java
+++ src/fr/thomapolis/polishub/gadgets/Gadget.java
@@ -1,5 +1,25 @@
 package fr.thomapolis.polishub.gadgets;
 
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
 public abstract class Gadget {
 
+	public abstract String name();
+	   
+    public abstract ItemStack item();
+   
+    public abstract int cooldown();
+   
+    public abstract void onInteract(Player player);
+   
+    public void give(Player player){
+        ItemStack it = item();
+        ItemMeta iM = it.getItemMeta();
+        iM.setDisplayName(name());
+        it.setItemMeta(iM);
+        player.getInventory().setItem(8, it);
+    }
+	
 }

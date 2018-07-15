cacb1167d193e249c50efe7ff24cbbdb72adf06d
thomapolis <thomapolis@gmail.com>
2018-07-15 20:09:58 +0200

    Work on Gadget

---
+++ src/fr/thomapolis/polishub/gadgets/Gadget.java
@@ -0,0 +1,5 @@
+package fr.thomapolis.polishub.gadgets;
+
+public abstract class Gadget {
+
+}
---
+++ src/fr/thomapolis/polishub/type/GadgetType.java
@@ -0,0 +1,24 @@
+package fr.thomapolis.polishub.type;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.thomapolis.polishub.gadgets.Gadget;
+
+public class GadgetType {
+
+	public List<Gadget> gadgets = new ArrayList<>();
+	
+	public GadgetType() {
+		
+		register();
+		
+	}
+	
+	public void register() {
+		
+		
+		
+	}
+	
+}

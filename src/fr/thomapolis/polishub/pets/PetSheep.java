9029a30be8637279647384f716e0fc97ae556601
thomapolis <thomapolis@gmail.com>
2018-07-18 21:13:34 +0200

    Work on petSheep

---
+++ src/fr/thomapolis/polishub/pets/PetSheep.java
@@ -0,0 +1,34 @@
+package fr.thomapolis.polishub.pets;
+
+import org.bukkit.entity.Player;
+
+import fr.thomapolis.polishub.api.IPet;
+import fr.thomapolis.polishub.api.IPetEvolutive;
+
+public class PetSheep implements IPet, IPetEvolutive {
+
+	@Override
+	public boolean baby() {
+		// XXX Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String name(Player player) {
+		// XXX Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void spawn(Player player) {
+		// XXX Auto-generated method stub
+		
+	}
+
+	@Override
+	public void followPlayer(Player player) {
+		// XXX Auto-generated method stub
+		
+	}
+
+}

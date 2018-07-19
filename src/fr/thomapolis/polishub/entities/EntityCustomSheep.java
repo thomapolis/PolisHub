c5c55b6918064756662583cc8740fa56c4a7db76
thomapolis <thomapolis@gmail.com>
2018-07-19 15:09:52 +0200

    Work on entity

--- src/fr/thomapolis/polishub/entities/EntityCustomSheep.java
+++ src/fr/thomapolis/polishub/entities/EntityCustomSheep.java
@@ -1,6 +1,7 @@
 package fr.thomapolis.polishub.entities;
 
 import net.minecraft.server.v1_11_R1.EntitySheep;
+import net.minecraft.server.v1_11_R1.NBTTagCompound;
 import net.minecraft.server.v1_11_R1.World;
 
 public class EntityCustomSheep extends EntitySheep {
@@ -15,7 +16,29 @@ public class EntityCustomSheep extends EntitySheep {
 		
 		this.setSilent(true);
 		this.setAI(true);
+		float speed = 0.2F;
+		this.l(speed);
 		
 		super.g(sideMot, forMot);
 	}
+	
+	public void f(double x, double y, double z) {}
+	
+	public void a(NBTTagCompound nbttagcompound) {
+    }
+
+    public void b(NBTTagCompound nbttagcompound) {
+    }
+
+    public boolean c(NBTTagCompound nbttagcompound) {
+        return false;
+    }
+
+    public boolean d(NBTTagCompound nbttagcompound) {
+        return false;
+    }
+
+    public NBTTagCompound e(NBTTagCompound nbttagcompound) {
+        return nbttagcompound;
+    }
 }
---
+++ src/fr/thomapolis/polishub/nms/register/RegisterCustomEntity.java
@@ -0,0 +1,54 @@
+package fr.thomapolis.polishub.nms.register;
+
+import fr.thomapolis.polishub.entities.EntityCustomSheep;
+import net.minecraft.server.v1_11_R1.Entity;
+import net.minecraft.server.v1_11_R1.EntitySheep;
+import net.minecraft.server.v1_11_R1.EntityTypes;
+import net.minecraft.server.v1_11_R1.MinecraftKey;
+
+public enum RegisterCustomEntity {
+
+	SHEEP("Sheep", 54, EntitySheep.class, EntityCustomSheep.class);
+	
+	private String name;
+    private int id;
+    private Class<? extends Entity> nmsClass;
+    private Class<? extends Entity> customClass;
+    private MinecraftKey key;
+    private MinecraftKey oldKey;
+
+    private RegisterCustomEntity(String name, int id, Class<? extends Entity> nmsClass, Class<? extends Entity> customClass) {
+        this.name = name;
+        this.id = id;
+        this.nmsClass = nmsClass;
+        this.customClass = customClass;
+        this.key = new MinecraftKey(name);
+        this.oldKey = EntityTypes.b.b(nmsClass);
+    }
+
+    public static void registerEntities() { for (RegisterCustomEntity ce : RegisterCustomEntity.values()) ce.register(); }
+    public static void unregisterEntities() { for (RegisterCustomEntity ce : RegisterCustomEntity.values()) ce.unregister(); }
+
+    private void register() {
+        EntityTypes.d.add(key);
+        EntityTypes.b.a(id, key, customClass);
+    }
+
+    private void unregister() {
+        EntityTypes.d.remove(key);
+        EntityTypes.b.a(id, oldKey, nmsClass);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public Class<?> getCustomClass() {
+        return customClass;
+    }
+	
+}

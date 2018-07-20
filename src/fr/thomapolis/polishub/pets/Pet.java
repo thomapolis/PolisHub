76dbb403e8e466f96dcd56f04665b6703be1b734
thomapolis <thomapolis@gmail.com>
2018-07-20 14:28:42 +0200

    Work on pets

--- src/fr/thomapolis/polishub/pets/Pet.java
+++ src/fr/thomapolis/polishub/pets/Pet.java
@@ -1,5 +1,38 @@
 package fr.thomapolis.polishub.pets;
 
-public abstract class Pet {
+import java.util.UUID;
 
+import org.bukkit.entity.Player;
+
+import fr.thomapolis.polishub.api.IPet;
+
+public abstract class Pet implements IPet{
+
+	public Player owner;
+	
+	public Pet(Player owner) {
+		
+		this.owner = owner;
+	}
+	
+	@Override
+	public Player getOwner() {
+		
+		return this.owner;
+	}
+	
+	@Override
+	public String getNameOfOwner() {
+		
+		return getOwner().getName();
+	}
+	
+	@Override
+	public UUID getOwnerUUID() {
+		
+		return getOwner().getUniqueId();
+	}
+	
+	
+	
 }
